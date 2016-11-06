acf689b386ecec995daa54fc2319935ab8961962
Ian Zhang <ianzhang1986@gmail.com>
2016-11-06 11:13:28 -0800

    [Feature] loaders and notes

---
+++ notes.js
@@ -0,0 +1,15 @@
+// jQuery preloading?
+$(document).ready(function(){
+  // will first fade out the loading animation
+  jQuery("#status").fadeOut(4000);
+        // will fade out the whole DIV that covers the website.
+  jQuery("#preloader").delay(1000).fadeOut("slow");
+  // jQuery(window).resize(function(){
+  //   resizeWin();
+  // });
+
+});
+
+// makes sure the whole site is loaded
+jQuery(window).load(function() {
+});
