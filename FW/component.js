d033a523b139f1d30d023694a4a7edb3fcaba363
Ian Zhang <ianzhang1986@gmail.com>
2016-11-08 20:12:28 -0800

    [Feature] Dyanmically load header portion

---
+++ FW/component.js
@@ -0,0 +1,17 @@
+// What header model would need?
+/**
+ *
+ */
+// TODO: design document structure
+
+// Header template
+// http://stackoverflow.com/questions/17636528/how-do-i-load-an-html-page-in-a-div-using-javascript
+// document.getElementById("content").innerHTML = '<>'
+// http://stackoverflow.com/questions/8988855/include-another-html-file-in-a-html-file
+
+var headerModel = new Skull.Model({
+  title: '',
+  completed: ''
+});
+
+document.getElementById('header').innerHTML = '<p>It is just a header</p>';
